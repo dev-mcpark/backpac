299bf57bf509960bedc9d0545f2588e48e42dc5c
dev-mcpark <maniya12345@gmail.com>
2020-06-08 21:05:50 +0900

    homework

--- backpac/settings.py
+++ backpac/settings.py
@@ -91,21 +91,21 @@ DATABASES = {
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
     'master': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'backpac',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
     },
     'slave': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'backpac',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
     },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'backpac',
-    #     'USER': 'user',
-    #     'PASSWORD': 'password',
-    #     'HOST': 'db',
-    #     'PORT': '3306',
-    # }
 }
 DATABASE_ROUTERS = ['backpac.routers.MasterSlaveRouter']
 
