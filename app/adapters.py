9eb08e6286d011cea9d03684d1b79f9fe45c1eef
dev-mcpark <maniya12345@gmail.com>
2020-06-09 23:35:18 +0900

    homework

--- app/adapters.py
+++ app/adapters.py
@@ -1,12 +1,8 @@
-import re
-
 from allauth.account.adapter import DefaultAccountAdapter
-from django import forms
 
 
 class MyAccountAdapter(DefaultAccountAdapter):
 
     def save_user(self, request, user, form, commit=True):
         user = super(MyAccountAdapter, self).save_user(request, user, form, commit)
-
         return user
